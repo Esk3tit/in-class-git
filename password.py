21a87fcc04e9dfd5ab6955f1e3cf086e3f050a97
Kai Pan <khaiphn41@gmail.com>
2021-04-15 19:58:07 -0700

    Finished the password generator

--- password.py
+++ password.py
@@ -1,10 +1,14 @@
 import string
 import random
 
-def password_gen(length):
 
+def password_gen(length):
     # Possible characters that are allowed in password
     # Includes upper and lower case letters, numbers, and punctuation
     possible_chars = string.ascii_letters + string.digits + string.punctuation
 
     # Use Python 3.6 random.choices to create password from possible characters
+    # Join the chars into a password string
+    print(''.join(random.choices(possible_chars, k=length)))
+
+
