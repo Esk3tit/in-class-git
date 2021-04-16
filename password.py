9429e278493cdc1430c6d32dd737f6ce5c96923f
Kai Pan <khaiphn41@gmail.com>
2021-04-15 19:56:30 -0700

    create possible characters string

--- password.py
+++ password.py
@@ -3,4 +3,8 @@ import random
 
 def password_gen(length):
 
-    pass
+    # Possible characters that are allowed in password
+    # Includes upper and lower case letters, numbers, and punctuation
+    possible_chars = string.ascii_letters + string.digits + string.punctuation
+
+    # Use Python 3.6 random.choices to create password from possible characters
