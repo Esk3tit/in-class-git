017d810ccda0cd31fe7a7de3d678a9f0a8a710ac
Kai Pan <khaiphn41@gmail.com>
2021-04-15 20:04:20 -0700

    Added print for calc

--- calc.py
+++ calc.py
@@ -4,6 +4,15 @@ def calc(a, b):
     diff = a - b
     mult = a * b
     div = a / b
+    print_calc(sum, diff, mult, div)
 
 
-# calc(3, 4)
+def print_calc(sum, diff, mult, div):
+    print(f"{sum}")
+    print(f"{diff}")
+    print(f"{mult}")
+    print(f"{div}")
+
+
+# Test code
+calc(3, 4)
