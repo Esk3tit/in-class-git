02f2bd117e385ca1d95aa00a93ace688e041c1e8
Kai Pan <khaiphn41@gmail.com>
2021-04-15 18:54:26 -0700

    added return value to return divisor list and added test run code

--- divisor.py
+++ divisor.py
@@ -4,9 +4,9 @@ def divisor(n):
 
     divisor_list = list()
 
-    # We because the range is exclusive on the maximum value,
-    # we end at sqrt(n) + 1 to include sqrt(n)
-    for i in range(1, int(math.sqrt(n) + 1)):
+    # Simple divisor that iterates up to n
+    # Not super efficient but gets the job done
+    for i in range(1, n + 1):
 
         if n % i == 0:
 
@@ -14,9 +14,16 @@ def divisor(n):
             # Add to divisor list
             divisor_list.append(i)
 
+    return divisor_list
+
 
 def print_divisor(lst):
 
     # Print divisor list
     for div in lst:
         print(f"{div} ")
+
+
+# Test run code
+div_list = divisor(100)
+print_divisor(div_list)
